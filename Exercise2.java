aff8480e005aced15a38922e5833c7cebcf7b0b7
Sahithi3110 <66991359+Sahithi3110@users.noreply.github.com>
2021-04-15 15:31:51 +0530

    Strings

--- Exercise2.java
+++ Exercise2.java
@@ -1,29 +1,15 @@
-package com.cg.Collections;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+package com.cg.lab3;
 
 public class Exercise2 {
 
 	public static void main(String[] args) {
-		char[] ch={'s','a','h','i','t','h','i'};
-		Map<Character,Integer> map=new HashMap<Character,Integer>();
-		map=countChars(ch);
-		for(Entry<Character, Integer>  m:map.entrySet())
-			System.out.println(m.getKey()+" "+m.getValue());
+		System.out.println(getImage ("EARTH"));
 
 	}
 
-	private static Map<Character,Integer> countChars(char[] ch) {
-		Map<Character,Integer> map=new HashMap<Character,Integer>();
-		for(int i=0;i<ch.length;i++)
-		{
-			if(map.containsKey(ch[i]))
-				map.put(ch[i], map.get(ch[i])+1);
-			else map.put(ch[i], 1);
-		}return map;
-		
+	private static String getImage(String string) {
+		StringBuffer str=new StringBuffer(string);
+		return str+"|"+str.reverse();
 	}
 
 }
